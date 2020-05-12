5ac2a4eb8da23c5c2a675d0444688e465f7fd409
Ryan Burgett <ryan@burgettdev.net>
2020-05-11 20:51:38 -0400

    Added file delete and copy link functionality

--- src/components/files/index.js
+++ src/components/files/index.js
@@ -1,4 +1,16 @@
 import { connect } from 'react-redux';
 import Files from './files';
+import { setUploads } from '../../actions/app-actions';
 
-export default connect()(Files);
+export default connect(
+  ({ appState }) => ({
+    accessKeyId: appState.accessKeyId,
+    secretAccessKey: appState.secretAccessKey,
+    region: appState.region,
+    s3Bucket: appState.s3Bucket,
+    uploads: appState.uploads
+  }),
+  dispatch => ({
+    setUploads: uploads => dispatch(setUploads(uploads))
+  })
+)(Files);
--- src/util/index.js
+++ src/util/index.js
@@ -1,5 +1,11 @@
+import { Alert } from 'react-native';
+
 export const handleError = err => {
   console.error(err);
+  Alert.alert(
+    'Oops!',
+    err.message
+  );
 };
 
 export const getDownloadLink = (bucket, key) => `https://s3.amazonaws.com/${bucket}/${encodeURI(key)}`;
